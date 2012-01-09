c80d85371fbea7762dba288142a4c59d3e8afb30
Marian Marinov <mm@yuhu.biz>
2012-01-09 08:04:49 +0200

    ipparser.h: Added CONF directive for the location of the IPs file

--- ipparser.h
+++ ipparser.h
@@ -7,6 +7,8 @@
 
 #define IP_RANGES { "11.0.0.0/24", "12.0.0.0/255.255.255.0", "13.1.0.12" }
 #define IP_COUNT 3
+#define CONF "ips.conf"
+
 struct myip {
 	struct in_addr net;
 	struct in_addr mask;
