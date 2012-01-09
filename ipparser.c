fadad379649bce0c3a78df0133c3f0ce66c63752
Marian Marinov <mm@yuhu.biz>
2012-01-09 14:04:47 +0200

    ipparser.c:  * Improved the int2bin() function    - removed the buffer and buffer size parameters    - allocate local char buffer so we can use it directly without allocating a buffer outside the function  * Moved the ip parser code into a separate function parse_ip() wich receives the string buffer and myip struct  * Added parse_file function, which will handle configuration file reads

--- ipparser.c
+++ ipparser.c
@@ -1,80 +1,118 @@
 #include "ipparser.h"
 
-char *int2bin(int a, char *buffer, int buf_size) {
+char int2bin(int a) {
 	int i;
-    buffer += (buf_size - 1);
+	char ipbuf[33];
+	ipbuf[33] = '\0';
     for (i = 31; i >= 0; i--) {
-        *buffer-- = (a & 1) + '0';
+        ipbuf[i] = (a & 1) + '0';
         a >>= 1;
     }
-    return buffer;
+    return ipbuf[33];
 }
 
-int main() {
-	char *p[] = IP_RANGES;
-	char *s = NULL;
-	char *f = NULL;
-	char ipbuf[33];
-	int i, m;
-	struct myip ip;
-	ipbuf[32] = '\0';
-
-	for(i=0; i<IP_COUNT; i++) {
-		printf("Net: %s\n", p[i]);
+int parse_ip(char *buf, struct myip *ip) {
+		char *s = NULL;
+		char *f = NULL;
+		int ipsize=0;
+		printf("Buf: %s\n", buf);
 		// Check for netmask and parse it
-		if ((s = strchr(p[i], '/'))) {
+		if ((s = strchr(buf, '/'))) {
 			// We have netmask
 			*s++;
 			if (strchr(s, '.')) {
-			// it is in the a.b.c.d form
-				printf("P: %s\n", s);
-				if (inet_aton(s, &ip.mask) == 0) {
+				// it is in the a.b.c.d form
+				if (inet_aton(s, &ip->mask) == 0) {
 					printf("Unable to parse the netmask\n");
 					return 1;
 				}
 			} else {
-			// it is in the /xx form
+				int m = 0;
+				// it is in the /xx form
 				m = atoi(s);
 				if (m > 32 || m <= 0) {
 					printf("invalid mask in network/netmask\n");
 					return 2;
 				}
-				ip.mask.s_addr = 0xFFFFFFFFUL << (32 - m);
-				ip.mask.s_addr = htonl(ip.mask.s_addr);
+				ip->mask.s_addr = 0xFFFFFFFFUL << (32 - m);
+				ip->mask.s_addr = htonl(ip->mask.s_addr);
 			}
-			printf("Mask: %d\n", ip.mask.s_addr);
-			int2bin((int) ip.mask.s_addr, ipbuf, 32);
-			printf("Mask(bin): %s\n", ipbuf);
+			printf("Mask: %d\n", ip->mask.s_addr);
+//			printf("Mask(bin): %s\n", int2bin((int) ip->mask.s_addr));
 		}
 		// Parse the IP portion
 		s = NULL;
-		f = NULL;
 		s = malloc(16);
 		memset(s, '\0', 16); // clear the memory
-		f = s;	// copy the pointer so we can print from the begining of the copied string
+		f = s;
 		
 		// copy the IP string
-		while (*p[i]) {
-			if (*p[i] == '/' || *p[i] == '\0')
+		while (*buf) {
+			if (*buf == '/' || *buf == '\0' || ipsize == 16)
 				break;
-			if (isdigit(*p[i]) || *p[i] == '.') {
-				*f = *p[i];
-				*p[i]++;
-				*f++;
+			if (isdigit(*buf) || *buf == '.') {
+				*s = *buf;
+				*buf++;
+				*s++;
+				ipsize++;
 			}
 		}
 		// convert the IP string into 32bit binary IP
-		if (inet_aton(s, &ip.net) == 0) {
+		if (inet_aton(f, &ip->net) == 0) {
 			printf("Unable to parse the IP\n");
 			return 4;
 		}
-		printf("IP: %d\n", ip.net.s_addr);
+		printf("IP: %d\n", ip->net.s_addr);
 // Print it in binary
 //		ipbuf[32] = '\0';
 //		int2bin((int) ip.net.s_addr, ipbuf, 32);
 //		printf("Mask(bin): %s\n", ipbuf);
 		printf("--------------------------------\n");
-	}
 	
+}
+
+int parse_file() {
+	FILE* fp;
+	char buf[1024];
+	char *a, *b;
+	int count = 0;
+	a = malloc(33);
+	b = a;
+	if ((fp = fopen(CONF,"rt")) == NULL) {
+		fprintf(stderr,"Error opening file: " CONF "\n");
+		return -1;
+	}
+	/* Read into the buffer contents within the file stream */
+	while(fgets(buf, 1024, fp) != NULL) {
+		b = buf;
+		memset(a, '\0', 33);
+		count=0;
+		printf("Buf:          %sChar by char: ", buf);
+		while(*b) {
+			if (*b == '\0' || *b == '\r' || *b == '\n')
+				break;
+			if (isdigit(*b) || *b == '.' || *b == '/') {
+				if (count == 32)
+					break;
+				printf("%c", *b);
+				*a = *b;
+				*a++;
+				count++;
+			}
+			*b++;
+		}
+		printf("\nSt: %s\n-------------------------\n", b);
+	}
+}
+
+int main() {
+	int i;
+	char *p[] = IP_RANGES;
+	struct myip *return_ip;
+	return_ip = malloc(sizeof(return_ip));
+	for(i=0; i<IP_COUNT; i++) {
+		parse_ip(p[i], return_ip);
+	}
+	parse_file();
 	return 0;
 }
